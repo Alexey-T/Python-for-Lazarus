ccb9dbb268ec65b6de064a2f95fb68ad9d6753de
Alexey-t <support@uvviewsoft.com>
2019-11-20 23:54:35 +0300

    sync RichCompare with pyscripter

--- python4lazarus/Sources/Core/PythonEngine.pas
+++ python4lazarus/Sources/Core/PythonEngine.pas
@@ -7867,9 +7867,25 @@ begin
   Result := 0;
 end;
 
+
 function  TPyObject.RichCompare( obj : PPyObject; Op : TRichComparisonOpcode) : PPyObject;
+Var
+  Res : Boolean;
 begin
-  Result := nil;
+  Res := False;
+  case Op of
+    pyLT: Res := Compare(obj) < 0;
+    pyLE: Res := Compare(obj) <= 0;
+    pyEQ: Res := Compare(obj) = 0;
+    pyNE: Res := Compare(obj) <> 0;
+    pyGT: Res := Compare(obj) > 0;
+    pyGE: Res := Compare(obj) >= 0;
+  end;
+  if Res then
+    Result := PPyObject(GetPythonEngine.Py_True)
+  else
+    Result := PPyObject(GetPythonEngine.Py_False);
+  GetPythonEngine.Py_INCREF( Result );
 end;
 
 function  TPyObject.Iter : PPyObject;
