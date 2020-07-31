6e70785a60ec2485b1bc4fa7bf4915c2658e29e5
Alexey <support@uvviewsoft.com>
2020-07-31 21:56:33 +0300

    sync VCL files

--- python4lazarus/Sources/VCL/PythonDatabase.pas
+++ python4lazarus/Sources/VCL/PythonDatabase.pas
@@ -22,7 +22,7 @@ interface
 uses Classes, PythonEngine;
 type
   {$IF not Defined(FPC) and (CompilerVersion >= 23)}
-  [ComponentPlatformsAttribute(pidWin32 or pidWin64)]
+  [ComponentPlatformsAttribute(pidSupportedPlatforms)]
   {$IFEND}
   TPythonDatabase = class(TEngineClient)
     protected
